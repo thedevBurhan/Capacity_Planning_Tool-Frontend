95d3e5ccc6cbfcdea592fa04ee91e31fe802ffff
thedevBurhan <smdburhan25@gmail.com>
2023-11-03 19:48:15 +0530

    url error

--- src/Components/Meeting/meeting.js
+++ src/Components/Meeting/meeting.js
@@ -11,7 +11,7 @@ import axios from "axios";
 // //configure thhe environment
 // dotenv.config();
 const CLIENTID = "Xu8dJsqkSEiUHAb4aFauA";
-const REDIRECTURL = encodeURIComponent("https://capacity-planning-tool.netlify.app/Meeting/zoom");
+const REDIRECTURL = encodeURIComponent("https://capacity-planning-tool.netlify.app/Meeting/zoom/");
 
 
 const Meeting = () => {
