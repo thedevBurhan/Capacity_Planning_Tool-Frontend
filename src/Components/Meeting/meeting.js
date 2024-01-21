f813992eadb77ee3112081a7d826167e3929a467
thedevBurhan <smdburhan25@gmail.com>
2024-01-21 21:01:25 +0530

    video call

--- src/Components/Meeting/meeting.js
+++ src/Components/Meeting/meeting.js
@@ -1,8 +1,78 @@
-import React  from "react";
-import DashBoard from "../DashBoard/Dashboard.js";
-import Groups3Icon from '@mui/icons-material/Groups3';
-import { Tooltip } from "@mui/material";
-import gifs from "./Zoom.gif";
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import DashBoard from '../DashBoard/Dashboard';
+
+
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+export function getUrlParams(
+  url = window.location.href
+) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function Meeting() {
+      const roomID = getUrlParams().get('roomID') || randomID(5);
+      let myMeeting = async (element) => {
+     // generate Kit Token
+      const appID =754103294;
+      const serverSecret = "59bcfa0b78b08b8f77222eb23fe3b697";
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
+
+
+     // Create instance object from Kit Token.
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      // start the call
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Copy link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+      });
+
+
+  };
+
+  return (
+    <div>
+      <DashBoard>
+        <div
+          className="myCallContainer"
+         ref={myMeeting}
+          style={{ width: '80vw', height: '80vh',marginLeft:"10%",marginTop:"-5%" }}
+           ></div>
+       </DashBoard>
+    </div>
+  );
+}
+
+
+// import DashBoard from "../DashBoard/Dashboard.js";
+// import Groups3Icon from '@mui/icons-material/Groups3';
+// import { Tooltip } from "@mui/material";
+// import gifs from "./Zoom.gif";
 // import Backdrop from '@mui/material/Backdrop';
 // import CircularProgress from '@mui/material/CircularProgress';
 // import Button from '@mui/material/Button';
@@ -10,33 +80,33 @@ import gifs from "./Zoom.gif";
 // //configure thhe environment
 // dotenv.config();
 
-const Meeting = () => {
+// const Meeting = () => {
 
-  const handleIconClick = () => {
-    const linkUrl = "https://us05web.zoom.us/j/8130158712?pwd=L59xJLy2NbCJzzDj9wDemNA7LnKYDk.1";
-    window.open(linkUrl, "_blank");
-  };
-  return (
-    <div>
-    <DashBoard>
-      <div className="addnew">
-      <Tooltip title="Click Me">
-        <h1 className="Heading" onClick={handleIconClick}> Create Meeting 
-        <span className="dash">----</span> <Groups3Icon/></h1>
-       </Tooltip>
-      </div>
-     <div className=" giff">
-     <img className="imggifs" src={gifs} alt="Planning" />
-     </div>
+//   const handleIconClick = () => {
+//     const linkUrl = "https://us05web.zoom.us/j/8130158712?pwd=L59xJLy2NbCJzzDj9wDemNA7LnKYDk.1";
+//     window.open(linkUrl, "_blank");
+//   };
+//   return (
+//     <div>
+//     <DashBoard>
+//       <div className="addnew">
+//       <Tooltip title="Click Me">
+//         <h1 className="Heading" onClick={handleIconClick}> Create Meeting 
+//         <span className="dash">----</span> <Groups3Icon/></h1>
+//        </Tooltip>
+//       </div>
+//      <div className=" giff">
+//      <img className="imggifs" src={gifs} alt="Planning" />
+//      </div>
           
         
-    </DashBoard>
-  </div>
-  )  
-};
+//     </DashBoard>
+//   </div>
+//   )  
+// };
 
 
-export default Meeting;
+// export default Meeting;
 
 // const Meeting = () => {
 //   // const [open, setOpen] = React.useState(false);
